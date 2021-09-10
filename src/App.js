1e4faec56e054ace1554865df05b42bf80cd8de1
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-09-09 23:51:29 -0500

    tailwind and style components add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import Home from "./page/Home";
+import "tailwindcss/dist/base.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/grafics_logo.svg" alt="logo" />
-        <img src="/create.png" alt="logo" />
-        <p className="App-link">PROXIMAMENTE</p>
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
